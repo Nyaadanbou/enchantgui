1f36066dac831a526a5b418c0f0fa1447d3a0e64
Akiranya <porridgen@gmail.com>
2024-01-09 06:15:09 +0800

    i18n lore poc

--- gui/src/main/kotlin/cc/mewcraft/enchantment/gui/gui/EnchantIcons.kt
+++ gui/src/main/kotlin/cc/mewcraft/enchantment/gui/gui/EnchantIcons.kt
@@ -10,7 +10,10 @@ import cc.mewcraft.enchantment.gui.util.wrapper
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
 import com.google.common.cache.LoadingCache
+import com.google.common.collect.ListMultimap
+import com.google.common.collect.MultimapBuilder.ListMultimapBuilder
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.ComponentLike
 import net.kyori.adventure.text.TranslatableComponent
 import net.kyori.adventure.text.TranslationArgument
 import xyz.xenondevs.invui.item.ItemProvider
@@ -115,6 +118,103 @@ class EnchantIcons
     // TODO 应该封装成一个 class 供其他项目复用
     // FIXME 下面是新的 i18n 实现，没测 runtime，仅供参考
 
+    class I18nLore(
+        private val format: List<String>,
+    ) {
+        private val sanitizers: HashMap<String, () -> Boolean> = HashMap()
+        private val registry: HashMap<String, TranslatableComponent> = HashMap() // FIXME 如果这里用 ListMultimap，那就写个扩展函数
+        private val replaced: ListMultimap<String, TranslatableComponent> = ListMultimapBuilder.hashKeys().arrayListValues().build()
+
+        /**
+         * 注册一个 [TranslatableComponent]，并将其 [TranslatableComponent.key] 设置为 [key]。
+         *
+         * @param key 将作为 [TranslatableComponent.key]
+         */
+        fun register(key: String) {
+            registry[key] = Component.translatable(key)
+        }
+
+        /**
+         * 跟 [register] 一样，只不过是一次性注册多个。
+         */
+        fun register(keys: Iterable<String>) {
+            keys.forEach { key -> register(key) }
+        }
+
+        /**
+         * 为指定的 [TranslatableComponent] 添加 [TranslationArgument]。
+         *
+         * @param key 翻译的键
+         * @param args 翻译的变量
+         * @throws IllegalArgumentException 如果 [key] 还未注册
+         */
+        fun arguments(key: String, args: List<ComponentLike>) {
+            val raw = requireNotNull(registry[key])
+            val new = raw.arguments(args)
+            replaced.put(key, new)
+        }
+
+        fun arguments(key: String, args: () -> List<ComponentLike>) = arguments(key, args())
+        fun arguments(key: String, vararg args: ComponentLike) = arguments(key, args.toList())
+        fun arguments(key: String, args: List<String>) = arguments(key, args.map(String::miniMessage))
+        fun arguments(key: String, args: () -> List<String>) = arguments(key, args())
+        fun arguments(key: String, vararg args: String) = arguments(key, args.toList())
+        fun arguments(key: String, args: () -> String) = arguments(key, args())
+
+        /**
+         * 为指定的 [TranslatableComponent] 添加 [TranslationArgument]。该函数会逐个将 [items]
+         * 中的元素作为 [TranslationArgument] 添加到 [key] 所对应的 [TranslatableComponent]
+         * 上，最终形成等同于 [items] 长度的一个列表 ([List]<[TranslatableComponent]>)。
+         *
+         * 例如，如果 [items] 中有3个元素，那么最终同一个 [key] 将对应3个 [TranslatableComponent]。这其中：
+         * - 第一个 [TranslatableComponent] 的变量为 [items] 中的第一个元素
+         * - 第二个 [TranslatableComponent] 的变量为 [items] 中的第二个元素
+         * - 第三个 [TranslatableComponent] 的变量为 [items] 中的第三个元素
+         *
+         * 最后，这3个 [TranslatableComponent] 都将在替换模板的时候被展开，也就是这3个
+         * [TranslatableComponent] 将替换掉模板中 [key] 所对应的内容。
+         *
+         * @param key 翻译的键
+         * @param items 翻译的参数
+         * @throws IllegalArgumentException 如果 [key] 还未注册
+         */
+        fun argumentsMany(key: String, items: List<ComponentLike>) {
+            // Hints: items.size 就是最终要复制的行数
+            val raw: TranslatableComponent = requireNotNull(registry[key]) // 要复制的 component
+            val map: List<TranslatableComponent> = items.map { raw.arguments(it) } // 只替换第一个遇到的 argument
+            replaced.putAll(key, map)
+        }
+
+        fun argumentsMany(key: String, items: () -> List<ComponentLike>) = argumentsMany(key, items())
+        fun argumentsMany(key: String, items: List<String>) = argumentsMany(key, items.map(String::miniMessage))
+        fun argumentsMany(key: String, items: () -> List<String>) = argumentsMany(key, items())
+
+        /**
+         * 注册一个清理逻辑。
+         *
+         * @param pattern 要移除的字符串模式（包含就算）
+         * @param predicate 该清理逻辑的执行前提条件
+         */
+        fun sanitize(pattern: String, predicate: () -> Boolean) {
+            sanitizers[pattern] = predicate
+        }
+
+        fun build(): List<TranslatableComponent> {
+            val format1 = format.toMutableList()
+
+            // 清理 format，隐藏没必要呈现的内容
+            for ((pattern, predicate) in sanitizers.entries) {
+                if (predicate.invoke()) {
+                    format1.removeIf { it.contains(pattern) }
+                }
+            }
+
+            // 根据清理后的 format，生成最终的 List<TranslatableComponent>
+            val componentLore = format1.flatMap { replaced[it] }
+            return componentLore
+        }
+    }
+
     private fun poc(enchant: UiEnchant, level: Int) {
         // 给定如下参数:
         // - UiEnchant (创建 TranslationArgument 所需要的任意对象)
@@ -122,6 +222,10 @@ class EnchantIcons
         // 我们的任务是根据 LoreFormat 和 UiEnchant
         // 生成一个 List<TranslatableComponent>
         // 用于设置物品的 lore
+
+        val i18nLore = I18nLore(
+            settings.LORE_FORMAT,
+        )
 
         /*
         第一步：读取所有的 TranslatableComponent
@@ -131,10 +235,8 @@ class EnchantIcons
         其中，string 将作为 map key；由其构建的 TranslatableComponent 将作为 map value
         然后将所有的数据放入一个 map (接下来叫这个 map 为 “registry”)
         */
-
-        val registry = HashMap<String, TranslatableComponent>() // FIXME 如果这里用 ListMultimap，那就写个扩展函数
-        registry.put(settings.DISPLAY_NAME_FORMAT)
-        registry.put(settings.LORE_FORMAT)
+        i18nLore.register(settings.DISPLAY_NAME_FORMAT)
+        i18nLore.register(settings.LORE_FORMAT)
 
         /*
         第二步：为部分 TranslatableComponent 添加 arguments
@@ -147,74 +249,54 @@ class EnchantIcons
         这样反倒可以让用户自行去自定义新的 i18n 条目
         */
 
-        registry.computeIfPresent("menu.enchantment.icon.display_name") { _, v -> v.arguments(enchant.displayName()[level].miniMessage()) }
-        // FIXME span multiple lines
-        // registry.computeIfPresent("menu.enchantment.icon.description") { _, v -> v.arguments() }
-        registry.computeIfPresent("menu.enchantment.icon.rarity") { _, v -> v.arguments(enchant.rarity().name.miniMessage()) }
-        registry.computeIfPresent("menu.enchantment.icon.target") { _, v ->
-            v.arguments(enchant.enchantmentTargets()
+        i18nLore.arguments("menu.enchantment.icon.display_name", enchant.displayName()[level]!!)
+        i18nLore.argumentsMany("menu.enchantment.icon.description", enchant.description()[level]!!)
+        i18nLore.arguments("menu.enchantment.icon.rarity", enchant.rarity().name)
+        i18nLore.arguments("menu.enchantment.icon.target") {
+            enchant.enchantmentTargets()
                 .map { targetTranslator.translate(it) }
-                .reduce { t1, t2 -> "$t1, $t2" }.miniMessage()
-            )
+                .reduce { t1, t2 -> "$t1, $t2" }
         }
-        registry.computeIfPresent("menu.enchantment.icon.level") { _, v ->
-            v.arguments(
-                TranslationArgument.numeric(enchant.minimumLevel()),
-                TranslationArgument.numeric(enchant.maximumLevel())
-            )
+        i18nLore.arguments("menu.enchantment.icon.level", TranslationArgument.numeric(enchant.minimumLevel()), TranslationArgument.numeric(enchant.maximumLevel()))
+        i18nLore.argumentsMany("menu.enchantment.icon.conflict.item") {
+            enchant.conflict()
+                .chunked(3)
+                .map { chunk ->
+                    chunk.map {
+                        it.name()
+                    }.reduce { e1, e2 ->
+                        "$e1, $e2"
+                    }
+                }
         }
-        // FIXME span multiple lines
-        // registry.computeIfPresent("menu.enchantment.icon.conflict.item") { _, v -> v.arguments() }
         if (enchant is Chargeable) {
-            registry.computeIfPresent("menu.enchantment.icon.charging.fuel") { _, v -> v.arguments(enchant.fuel.miniMessage()) }
-            registry.computeIfPresent("menu.enchantment.icon.conflict.consume_amount") { _, v -> v.arguments(TranslationArgument.numeric(enchant.fuelConsume[level]!!)) }
-            registry.computeIfPresent("menu.enchantment.icon.conflict.recharge_amount") { _, v -> v.arguments(TranslationArgument.numeric(enchant.fuelRecharge[level]!!)) }
-            registry.computeIfPresent("menu.enchantment.icon.conflict.max_amount") { _, v -> v.arguments(TranslationArgument.numeric(enchant.maximumFuel[level]!!)) }
+            i18nLore.arguments("menu.enchantment.icon.charging.fuel", enchant.fuel)
+            i18nLore.arguments("menu.enchantment.icon.conflict.consume_amount", TranslationArgument.numeric(enchant.fuelConsume[level]!!))
+            i18nLore.arguments("menu.enchantment.icon.conflict.recharge_amount", TranslationArgument.numeric(enchant.fuelRecharge[level]!!))
+            i18nLore.arguments("menu.enchantment.icon.conflict.max_amount", TranslationArgument.numeric(enchant.maximumFuel[level]!!))
         }
-        registry.computeIfPresent("menu.enchantment.icon.obtaining.enchanting") { _, v -> v.arguments(TranslationArgument.numeric(enchant.enchantingChance())) }
-        registry.computeIfPresent("menu.enchantment.icon.obtaining.villager") { _, v -> v.arguments(TranslationArgument.numeric(enchant.villagerTradeChance())) }
-        registry.computeIfPresent("menu.enchantment.icon.obtaining.loot_generation") { _, v -> v.arguments(TranslationArgument.numeric(enchant.lootGenerationChance())) }
-        registry.computeIfPresent("menu.enchantment.icon.obtaining.fishing") { _, v -> v.arguments(TranslationArgument.numeric(enchant.fishingChance())) }
-        registry.computeIfPresent("menu.enchantment.icon.obtaining.mob_spawning") { _, v -> v.arguments(TranslationArgument.numeric(enchant.mobSpawningChance())) }
+        i18nLore.arguments("menu.enchantment.icon.obtaining.enchanting", TranslationArgument.numeric(enchant.enchantingChance()))
+        i18nLore.arguments("menu.enchantment.icon.obtaining.villager", TranslationArgument.numeric(enchant.villagerTradeChance()))
+        i18nLore.arguments("menu.enchantment.icon.obtaining.loot_generation", TranslationArgument.numeric(enchant.lootGenerationChance()))
+        i18nLore.arguments("menu.enchantment.icon.obtaining.fishing", TranslationArgument.numeric(enchant.fishingChance()))
+        i18nLore.arguments("menu.enchantment.icon.obtaining.mob_spawning", TranslationArgument.numeric(enchant.mobSpawningChance()))
 
         /*
         第三步：清理 lore format，隐藏没必要呈现的内容
         */
-        val sanitizedLoreFormat = sanitizeLoreFormat(enchant, settings.LORE_FORMAT)
+        // 对于无法充能的附魔，隐藏充能相关的描述
+        i18nLore.sanitize("charging") { enchant !is Chargeable }
+        // 对于为0的出现概率，移除对应种类的描述
+        i18nLore.sanitize("obtaining.enchanting") { enchant.enchantingChance() <= 0 }
+        i18nLore.sanitize("obtaining.villager") { enchant.villagerTradeChance() <= 0 }
+        i18nLore.sanitize("obtaining.loot_generation") { enchant.lootGenerationChance() <= 0 }
+        i18nLore.sanitize("obtaining.fishing") { enchant.fishingChance() <= 0 }
+        i18nLore.sanitize("obtaining.mob_spawning") { enchant.mobSpawningChance() <= 0 }
 
         /*
         第四步：根据清理后的 lore format，生成对应的 List<TranslatableComponent>
         */
-        val componentName = registry[settings.DISPLAY_NAME_FORMAT]!!
-        val componentLore = sanitizedLoreFormat.map { registry[it]!! } // FIXME span multiple lines, we might need flatMap
-
-        // 最后把 name 和 lore 应用到 ItemBuilder 上就行了
-    }
-
-    private fun HashMap<String, TranslatableComponent>.put(key: String) {
-        put(key, Component.translatable(key))
-    }
-
-    private fun HashMap<String, TranslatableComponent>.put(keys: List<String>) {
-        keys.forEach { key -> put(key, Component.translatable(key)) }
-    }
-
-    private fun sanitizeLoreFormat(enchant: UiEnchant, format: List<String>): MutableList<String> {
-        val format1 = format.toMutableList()
-        fun sanitize(pattern: String, predicate: () -> Boolean) {
-            if (predicate.invoke()) {
-                format1.removeIf { it.contains(pattern) }
-            }
-        }
-        // 对于无法充能的附魔，隐藏充能相关的描述
-        sanitize("charging") { enchant !is Chargeable }
-        // 对于为0的出现概率，移除对应种类的描述
-        sanitize("obtaining.enchanting") { enchant.enchantingChance() <= 0 }
-        sanitize("obtaining.villager") { enchant.villagerTradeChance() <= 0 }
-        sanitize("obtaining.loot_generation") { enchant.lootGenerationChance() <= 0 }
-        sanitize("obtaining.fishing") { enchant.fishingChance() <= 0 }
-        sanitize("obtaining.mob_spawning") { enchant.mobSpawningChance() <= 0 }
-        return format1
+        val ret = i18nLore.build()
     }
 
     ///////////////////////////////////////////////
